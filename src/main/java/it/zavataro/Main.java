c4116de6b9045a326e3d2a323c1f65a543e6eed9
lai2-24 <lai2-24@itismeucci.com>
2024-10-28 13:26:49 +0100

    Final Commit

--- src/main/java/it/zavataro/Main.java
+++ src/main/java/it/zavataro/Main.java
@@ -1,7 +1,42 @@
 package it.zavataro;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Main {    
+    public static ArrayList<String> usernames = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            ServerSocket ss = new ServerSocket(3000);
+            System.out.println("Server Avviato");
+            do {
+                Socket s = ss.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                DataOutputStream out = new DataOutputStream(s.getOutputStream());
+                System.out.println("Client Connesso");
+                String username = "";
+                while (true) {
+                    username = in.readLine();
+                    if (usernames.contains(username)) {
+                        out.writeBytes("CHA" + "\n");
+                    } else {
+                        out.writeBytes("REG" + "\n");
+                        usernames.add(username);
+                        break;
+                    }
+                }
+                System.out.println("User Client : " + username);
+                MyThread t = new MyThread(s,username);
+                t.start();
+            } while (true);
+        } catch (IOException e) {
+            System.out.println("ERRORE");
+            System.exit(1);
+        }
     }
 }
--- src/main/java/it/zavataro/MyThread.java
+++ src/main/java/it/zavataro/MyThread.java
@@ -1,5 +1,55 @@
 package it.zavataro;
 
-public class MyThread {
-    
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class MyThread extends Thread{
+    private Socket mySocket;
+    ArrayList<String> list;
+    String username;
+
+    public MyThread(Socket s, String u) {
+        mySocket = s;
+        list = new ArrayList<>();
+        username = u;
+    }
+
+    @Override
+    public void run() {
+        String stringRed = "";
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+                DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
+                do {    
+                    stringRed = in.readLine();
+                    if (stringRed.equals("#")) {
+                        String delete = in.readLine().toLowerCase();
+                        if (list.contains(delete)) {
+                            list.remove(delete);
+                            out.writeBytes("OK" + "\n");
+                        } else {
+                            out.writeBytes("NO" + "\n");
+                        }
+                    } else if (stringRed.equals("?")) {
+                        for (int i = 0; i < list.size(); i++) {
+                            out.writeBytes(list.get(i) + "\n");
+                        }
+                        out.writeBytes("@" + "\n");
+                    } else if (stringRed.equals("!")) {
+                        mySocket.close();
+                        break;
+                    } else {
+                        list.add(stringRed.toLowerCase());
+                        out.writeBytes("OK" + "\n");
+                    }
+                } while(true);
+            } catch (Exception e) {
+                System.err.println("ERRORE");
+                System.exit(1);
+            }
+        super.run();
+    }
 }
